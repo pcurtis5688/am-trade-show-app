83a0bc3280add7c6e84f9adcb4e8dac68a858b98
pcurtis5688 <pcurtis5688@gmail.com>
2016-08-27 12:28:33 -0400

    COMMIT AUG 27 AT THIS POINT I NEED TO SWAP CONTENT VIA THE HOME ACTIVITY

--- app/src/main/java/com/ashtonmansion/tradeshowmanagement/activity/EmailConfirmation.java
+++ app/src/main/java/com/ashtonmansion/tradeshowmanagement/activity/EmailConfirmation.java
@@ -1,10 +1,11 @@
-package com.ashtonmansion.tradeshowmanagement;
+package com.ashtonmansion.tradeshowmanagement.activity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,20 +13,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.ashtonmansion.amtradeshowmanagement.R;
-import com.ashtonmansion.tradeshowmanagement.activity.EmailConfirmation;
-import com.ashtonmansion.tradeshowmanagement.activity.MakeReservation;
 
-public class HomeActivity extends AppCompatActivity
+public class EmailConfirmation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    private Context homeActivityContext;
+    private Context emailConfirmationActivityContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        homeActivityContext = this;
-        setContentView(R.layout.activity_home);
+        emailConfirmationActivityContext = this;
+        setContentView(R.layout.activity_email_res_confirmation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -80,36 +80,31 @@ public class HomeActivity extends AppCompatActivity
         if (id == R.id.nav_home_btn) {
             //// TODO: 8/27/2016
         } else if (id == R.id.nav_show_setup_btn) {
-            //// TODO: 8/27/2016  
+            //// TODO: 8/27/2016
         } else if (id == R.id.nav_config_booths_btn) {
             //// TODO: 8/27/2016
         } else if (id == R.id.nav_reports_queries_btn) {
-            //// TODO: 8/27/2016  
+            //// TODO: 8/27/2016
         } else if (id == R.id.nav_make_reservation_btn) {
-            Intent makeReservationIntent = new Intent(homeActivityContext, MakeReservation.class);
+            Intent makeReservationIntent = new Intent(emailConfirmationActivityContext, MakeReservation.class);
             startActivity(makeReservationIntent);
         } else if (id == R.id.nav_email_reservation_confirmation_btn) {
-            Intent emailConfirmationIntent = new Intent(homeActivityContext, EmailConfirmation.class);
+            Intent emailConfirmationIntent = new Intent(emailConfirmationActivityContext, EmailConfirmation.class);
             startActivity(emailConfirmationIntent);
         } else if (id == R.id.nav_advertising_sales_btn) {
-            //// TODO: 8/27/2016  
+            //// TODO: 8/27/2016
         } else if (id == R.id.nav_general_tix_sales_btn) {
-            //// TODO: 8/27/2016  
+            //// TODO: 8/27/2016
         } else if (id == R.id.nav_special_events_sales_btn) {
-            //// TODO: 8/27/2016  
+            //// TODO: 8/27/2016
         } else if (id == R.id.nav_merchandise_sales_btn) {
-            //// TODO: 8/27/2016  
+            //// TODO: 8/27/2016
         } else if (id == R.id.nav_app_settings_btn) {
-
+            //// TODO: 8/27/2016
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    public void makeReservation(View view) {
-        Intent makeReservationIntent = new Intent(this, MakeReservation.class);
-        startActivity(makeReservationIntent);
     }
 }
