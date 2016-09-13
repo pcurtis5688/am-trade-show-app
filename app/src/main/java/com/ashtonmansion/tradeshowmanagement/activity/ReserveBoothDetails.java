8693ea82d4b95eecc7e386bbc9fc654664b472dd
pcurtis5688 <pcurtis5688@gmail.com>
2016-09-13 13:47:03 -0400

    13 September - New customer fields look good, existing customer has a filter search by last name. Just need to implement on-click method for that.

--- app/src/main/java/com/ashtonmansion/tradeshowmanagement/activity/ReserveBoothDetails.java
+++ app/src/main/java/com/ashtonmansion/tradeshowmanagement/activity/ReserveBoothDetails.java
@@ -1,28 +1,45 @@
 package com.ashtonmansion.tradeshowmanagement.activity;
 
+import android.accounts.Account;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.ashtonmansion.amtradeshowmanagement.R;
 import com.ashtonmansion.tradeshowmanagement.util.GlobalUtils;
+import com.clover.sdk.util.CloverAccount;
+import com.clover.sdk.v1.BindingException;
+import com.clover.sdk.v1.ClientException;
+import com.clover.sdk.v1.ServiceException;
+import com.clover.sdk.v1.customer.CustomerConnector;
+import com.clover.sdk.v1.customer.Customer;
 import com.clover.sdk.v3.inventory.Category;
 import com.clover.sdk.v3.inventory.Item;
 import com.clover.sdk.v3.inventory.Tag;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ReserveBoothDetails extends AppCompatActivity {
     private Context reserveBoothDetailsActivityContext;
+    ///////CLOVER DATA
+    private Account merchantAccount;
+    private CustomerConnector customerConnector;
     ///////SHOW DATA
     private Category show;
     private String showID;
@@ -36,6 +53,8 @@ public class ReserveBoothDetails extends AppCompatActivity {
     private Tag sizeTag;
     private Tag areaTag;
     private Tag categoryTag;
+    ///////CUSTOMER DATA
+    private List<Customer> existingCustomers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +82,8 @@ public class ReserveBoothDetails extends AppCompatActivity {
             populateTagFields();
         }
         //////////SET UP CUSTOMER-RELATED FIELDS
+        GetCustomerListTask getCustomerListTask = new GetCustomerListTask();
+        getCustomerListTask.execute();
         setupCustomerFields();
     }
 
@@ -83,8 +104,6 @@ public class ReserveBoothDetails extends AppCompatActivity {
                 }
             }
         });
-
-        EditText searchExistingCustomersField = (EditText) findViewById(R.id.booth_reservation_search_existing_customers_field);
     }
 
     private void decoupleShowName(Category show) {
@@ -144,4 +163,77 @@ public class ReserveBoothDetails extends AppCompatActivity {
         }
     }
 
+    private class GetCustomerListTask extends AsyncTask<Void, Void, Void> {
+        //////////PRIVATELY NECESSARY OBJECTS & UTILITY LISTS ONLY
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(reserveBoothDetailsActivityContext);
+            progressDialog.setMessage(getResources().getString(R.string.booth_reservation_loading_existing_customers_text));
+            progressDialog.show();
+            existingCustomers = new ArrayList<>();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                //////CONNECT CLOVER ACCT & CUSTOMER CONNECTOR
+                merchantAccount = CloverAccount.getAccount(reserveBoothDetailsActivityContext);
+                customerConnector = new CustomerConnector(reserveBoothDetailsActivityContext, merchantAccount, null);
+                customerConnector.connect();
+
+                existingCustomers = customerConnector.getCustomers();
+            } catch (RemoteException | BindingException | ServiceException | ClientException e1) {
+                Log.e("Clover Excptn; ", e1.getClass().getName() + " : " + e1.getMessage());
+            } finally {
+                customerConnector.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            populateExistingCustomersListview();
+            progressDialog.dismiss();
+        }
+    }
+
+    private void populateExistingCustomersListview() {
+        ////////ITERATE OVER CUSTOMER LIST, CREATE STRING LIST
+        List<String> existingCustomerStrings = new ArrayList<>();
+        for (Customer currentCustomer : existingCustomers) {
+            String customerString = currentCustomer.getLastName() + "," + currentCustomer.getFirstName() + " (" + currentCustomer.getId() + ")";
+            existingCustomerStrings.add(customerString);
+        }
+
+        ////////SET ARRAYADAPTER TO LISTVIEW
+        ListView existingCustomersListview = (ListView) findViewById(R.id.existing_customers_lv);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                existingCustomerStrings);
+        existingCustomersListview.setAdapter(arrayAdapter);
+
+        ///////GET SEARCH HANDLER AND ADD LISTENER
+        EditText searchExistingCustomersField = (EditText) findViewById(R.id.booth_reservation_search_existing_customers_field);
+        searchExistingCustomersField.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                arrayAdapter.getFilter().filter(cs);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+            }
+        });
+    }
 }
