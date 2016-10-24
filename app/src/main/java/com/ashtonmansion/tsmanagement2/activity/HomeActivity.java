237f8b10f0cd70c58dcb9cb939f7babb7c09aa69
pcurtis5688 <pcurtis5688@gmail.com>
2016-10-23 20:47:28 -0400

    Just finalized updates requested 23 October and uploaded APK . Commit and push before more work done.

--- app/src/main/java/com/ashtonmansion/tsmanagement2/activity/HomeActivity.java
+++ app/src/main/java/com/ashtonmansion/tsmanagement2/activity/HomeActivity.java
@@ -11,10 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.ashtonmansion.tsmanagement2.R;
-import com.ashtonmansion.tsmanagement2.util.GlobalUtils;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
